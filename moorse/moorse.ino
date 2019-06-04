e699e35998e1b80d6a38fa55065f04dacd3f29fe
yli12138 <ryanli020709@gmail.com>
2019-06-04 13:11:20 -0400

    push button

--- moorse/moorse.ino
+++ moorse/moorse.ino
@@ -1,11 +1,31 @@
 #include <LiquidCrystal.h>
+
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+const int push = 0, de = 1;
+const int ledPin = 8;
 
 void setup() {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
+
+  pinMode(push, INPUT);
+  pinMode(de, INPUT);
+
+  //pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  buttonState = digitalRead(push);
+  
+  if (buttonState == HIGH) {
+    //digitalWrite(ledPin, HIGH);
+    lcd.setCursor(0, 1);
+    lcd.print(pushing);
+  } else {
+    //digitalWrite(ledPin, LOW);
+    lcd.setCursor(0, 1);
+    lcd.print(released);
+  }
 }
