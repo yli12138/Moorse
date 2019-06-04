812bb2d28f5497584703ddbf5a34c7628031a668
yli12138 <ryanli020709@gmail.com>
2019-06-04 12:43:36 -0400

    Deleted seconds code

--- CODE/CODE.ino
+++ CODE/CODE.ino
@@ -8,9 +8,4 @@ void setup() {
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 0);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
 }
