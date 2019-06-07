a49358579b9b43c1d8dc18047f1c6b267329d402
yli12138 <ryanli020709@gmail.com>
2019-06-07 14:15:32 -0400

    hahahahah

--- moorse/moorse.ino
+++ moorse/moorse.ino
@@ -4,28 +4,33 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 const int sendButton = 1;
-const int ledPin = 8;
-
-int buttonState = 0;
-int flexVal = 0;
+int sendButtonState = 0;
 
 String currentLine = "";
 String oldLine = "";
 
-const int unit = 500;
+boolean finish = false;
+
+const unsigned long unit = 50;
+const int flexThreshold = 750;
+
+boolean flexPressing = false;
+
+unsigned long millisNow = 0;
 
 void sendLine(){
-  oldLine = currentLine;
+  oldLine = "" + currentLine;
   currentLine = "";
 
+//  lcd.clear();
   lineUpDate();
 }
 
 void lineUpDate(){//call after sendLine
-  lcd.clear();
-  
   lcd.setCursor(0,0);
   lcd.print(oldLine);
+  lcd.setCursor(0,1);
+  lcd.print(currentLine);
 }
 
 void setup() {
@@ -35,16 +40,59 @@ void setup() {
   pinMode(sendButton, INPUT);
 
   //pinMode(ledPin, OUTPUT);
+//  Serial.begin(9600);
 }
 
 void loop() {
-  buttonState = digitalRead(sendButton);
-  
-  if (buttonState == HIGH) {
-    lcd.clear();
+  sendButtonState = digitalRead(sendButton);
+  int flexVal = analogRead(A0);
+
+  if(flexVal <= flexThreshold) {
+    // Pressing
+    if(!flexPressing) {
+      flexPressing = true;
+      millisNow = millis();
+    }
   } else {
-    //digitalWrite(ledPin, LOW);
-    lcd.setCursor(0,0);
-    flexVal = analogRead(A0);
-    lcd.print(flexVal);
+    if(flexPressing && !finish) {
+      finish = true;
+      flexPressing = false;
+      int typeOfPress = determinePressType(millis() - millisNow);
+      if(typeOfPress == -1) {
+        currentLine = "invalid!";
+        lineUpDate();
+      } else if(typeOfPress == 0) {
+        currentLine = "short press!";
+        lineUpDate();
+      } else {
+        currentLine = "long press!";
+        lineUpDate();
+      }
+    }
+  }
+
+//  if(flexPressing) {
+//    lcd.setCursor(0,0);
+//    lcd.print("pressing!");
+//  } else {
+//    lcd.clear();
+//  }
+  
+  if (sendButtonState == LOW) {
+    finish = false;
+    sendLine();
+  } 
+  //else {
+//    //digitalWrite(ledPin, LOW);
+//    lcd.setCursor(0,0);
+//    lcd.print(flexVal);
+//  }
 }
+
+// returns 0 for short press, 1 for long press, -1 for invalid
+int determinePressType(unsigned long millisDiff) {
+  if(millisDiff >= 3*unit) return 1;
+  if(millisDiff >= unit && millisDiff < 3*unit) return 0;
+  return -1;
+}
+
