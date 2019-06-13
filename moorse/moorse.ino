43545d7fce6758142070549324759117c09711f0
Mingle Li <limingle5@gmail.com>
2019-06-13 10:54:30 -0400

    Delete code -- untestedgit add .

--- moorse/moorse.ino
+++ moorse/moorse.ino
@@ -9,16 +9,86 @@ boolean clearedBuffer = true;
 
 const unsigned long unit = 150;
 const unsigned long space = 2000;
+const unsigned long deleteTime = 2000;
 const int flexThreshold = 750;
 const int deleteThreshold = 880;
 
 boolean flexPressing = false;
+boolean flexDeleting = false;
 
 unsigned long millisNow = 0;
 unsigned long millisPause = 0;
+unsigned long millisDelete = 0;
 
 int morseBuffer[5];
 int morseBufferIndex = 0;
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+
+  //pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  if (clearedBuffer == false && millis() - millisPause >= space) {
+    Serial.println("Clearing!");
+    pushMorseBuffer();
+    clearedBuffer = true;
+  }
+  
+  int flexVal = analogRead(A0);
+
+  if (flexVal <= flexThreshold) {
+    // Pressing
+    if (flexPressing == false) {
+      Serial.println("Pressing");
+      flexPressing = true;
+      millisNow = millis();
+      millisPause = millis();
+    }
+  } else if (flexVal >= deleteThreshold) {
+    // Deleting
+    if(flexDeleting == false) {
+      flexDeleting = true;
+      millisDelete = millis();
+    } else {
+      if(millis() - millisDelete >= deleteTime) {
+        deleteLastLetter();
+        flexDeleting = false;
+      }
+    }
+  } else {
+    if (flexPressing == true) {
+      Serial.println("Released");
+      int typeOfPress = determinePressType(millis() - millisNow);
+      if (typeOfPress != -1) {
+        Serial.println("Valid value: " + String(typeOfPress));
+        morseBuffer[morseBufferIndex] = typeOfPress;
+        morseBufferIndex += 1;
+        clearedBuffer = false;
+      } else {
+        // invalid press
+      }
+
+      flexPressing = false;
+
+      millisPause = millis();
+    }
+
+    if(flexDeleting == true) {
+      flexDeleting = false;
+    }
+  }
+}
+
+// returns 1 for short press, 2 for long press, -1 for invalid
+int determinePressType(unsigned long millisDiff) {
+  if (millisDiff >= 3 * unit) return 2;
+  if (millisDiff >= unit && millisDiff < 3 * unit) return 1;
+  return -1;
+}
 
 void pushLetter(String letter) {
   Serial.println("Pushing " + letter + ", index " + cursorIndex);
@@ -27,31 +97,10 @@ void pushLetter(String letter) {
   cursorIndex++;
 }
 
-void sendLine() {
-  //  oldLine = "";
-  //  oldLine.concat(currentLine);
-  //  currentLine = "";
-
-  //lcd.clear();
-  //  lineUpDate();
-}
-
-void lineUpDate() { //call after sendLine
-  Serial.println("Called!");
-  lcd.setCursor(0, 0);
-  //  Serial.println("Old Line: " + String(oldLine));
-  //  lcd.print(String(oldLine));
-  lcd.setCursor(0, 1);
-  //  lcd.print(String(currentLine));
-  //  Serial.println("Current: " + String(currentLine));
-}
-
-void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-
-  //pinMode(ledPin, OUTPUT);
-  Serial.begin(9600);
+void deleteLastLetter() {
+  lcd.setCursor(cursorIndex - 1, 0);
+  lcd.print(" ");
+  cursorIndex--;
 }
 
 void pushMorseBuffer() {
@@ -179,70 +228,5 @@ void pushMorseBuffer() {
 
   memset(morseBuffer, 0, sizeof(morseBuffer));
   morseBufferIndex = 0;
-}
-
-void loop() {
-  if (clearedBuffer == false && millis() - millisPause >= space) {
-    Serial.println("Clearing!");
-    pushMorseBuffer();
-    clearedBuffer = true;
-  }
-
-  //  sendButtonState = ;
-  //  Serial.println("Button: " + String(digitalRead(sendButton)));
-  int flexVal = analogRead(A0);
-
-  if (flexVal <= flexThreshold) {
-    // Pressing
-    if (flexPressing == false) {
-      Serial.println("Pressing");
-      flexPressing = true;
-      millisNow = millis();
-      millisPause = millis();
-    }
-  } else {
-    if (flexPressing == true) {
-      Serial.println("Released");
-      int typeOfPress = determinePressType(millis() - millisNow);
-      if (typeOfPress != -1) {
-        Serial.println("Valid value: " + String(typeOfPress));
-        morseBuffer[morseBufferIndex] = typeOfPress;
-        morseBufferIndex += 1;
-        clearedBuffer = false;
-      } else {
-        // invalid press
-      }
-
-      //      lineUpDate();
-
-      flexPressing = false;
-
-      millisPause = millis();
-    }
-  }
-
-  //  if(flexPressing) {
-  //    lcd.setCursor(0,0);
-  //    lcd.print("pressing!");
-  //  } else {
-  //    lcd.clear();
-  //  }
-
-  //  if (sendButtonState == HIGH) {
-  //    Serial.println("Button pressed!");
-  //    sendLine();
-  //  }
-  //  else { //debugging code
-  //    //digitalWrite(ledPin, LOW);
-  //    lcd.setCursor(0,0);
-  //    lcd.print(flexVal);
-  //  }
-}
-
-// returns 1 for short press, 2 for long press, -1 for invalid
-int determinePressType(unsigned long millisDiff) {
-  if (millisDiff >= 3 * unit) return 2;
-  if (millisDiff >= unit && millisDiff < 3 * unit) return 1;
-  return -1;
 }
 
